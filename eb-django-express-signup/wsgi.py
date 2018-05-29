53757d59fb6ce14a1cc4dba461ab5d73e5501207
Ozge Koroglu <36756842+ozgekoroglu@users.noreply.github.com>
2018-05-29 11:04:07 +0200

    wsgi troubleshoot

--- eb-django-express-signup/wsgi.py
+++ eb-django-express-signup/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eb-django-express-signup/eb-django-express-signup.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eb-django-express-signup/eb-django-express-signup.settings")
 
 application = get_wsgi_application()
